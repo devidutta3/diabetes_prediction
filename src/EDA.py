1b9fd01cbaa4b32a859e2c56a1cb220ef8e27ec0
dasdevidutta3@gmail.com <dasdevidutta3@gmail.com>
2026-06-21 23:06:10 +0530

    How many diabetic patients? How many non-diabetic patients?

--- src/EDA.py
+++ src/EDA.py
@@ -1,3 +1,23 @@
-from load_dataset import df
+import pandas as pd
 
-def statics_review(df)
+def statics_review(df):
+    print("The Info Of The DataSets Are:\n")
+    df.info()
+    print("The Stastical Review Of The DataSets Are:\n")
+    print(df.describe())
+
+
+ 
+
+def check_zero_values(df):
+    cols = ["Glucose","BloodPressure","SkinThickness","Insulin","BMI"]
+
+    print("\nZero Value Analysis:\n")
+
+    for col in cols:
+        print(f"{col}: {(df[col] == 0).sum()}")
+
+df=pd.read_csv(r"data\diabetes.csv")
+statics_review(df) 
+check_zero_values(df)
+print(df["Outcome"].value_counts())
