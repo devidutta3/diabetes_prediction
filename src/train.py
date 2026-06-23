867e75f3acb6f71002ab9f616046986ad14976e2
dasdevidutta3@gmail.com <dasdevidutta3@gmail.com>
2026-06-23 23:18:23 +0530

    Done

--- src/train.py
+++ src/train.py
@@ -1,20 +1,51 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split as tt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+# Load cleaned dataset
+df = pd.read_csv(r"data\clean_dataset.csv")
 
-#Load The Clean DataSet
-df=pd.read_csv(r"data\clean_dataset.csv")
-print("Data Set Loaded Successfully:\n")
-print(df.head(10))
-X=df.drop("Outcome",axis=1)
-y=df["Outcome"]
-def train_split():
-    X_train,X_test,y_train,y_test=tt(
-        X,
-        y,
-        test_size=0.2,
-        random_state=42
-    )
-    return X_train.shape,X_test.shape,y_train.shape,y_test.shape
+# Features (Inputs)
+X = df.drop("Outcome", axis=1)
 
-result=train_split()
-print(result,end="\n")
+# Target (Output)
+y = df["Outcome"]
+
+# Split dataset into Train and Test
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+print("Before Scaling")
+print("X_train Shape:", X_train.shape)
+print("X_test Shape :", X_test.shape)
+print("y_train Shape:", y_train.shape)
+print("y_test Shape :", y_test.shape)
+
+print("\n" + "---" * 30)
+
+# Create Scaler Object
+scaler = StandardScaler()
+
+# Learn mean/std from training data and scale it
+X_train = scaler.fit_transform(X_train)
+
+# Use same mean/std on testing data
+X_test = scaler.transform(X_test)
+
+print("After Scaling")
+print("X_train Shape:", X_train.shape)
+print("X_test Shape :", X_test.shape)
+
+print("\nFirst 5 Rows of Scaled X_train:\n")
+print(X_train[:5])
+
+model=LogisticRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
